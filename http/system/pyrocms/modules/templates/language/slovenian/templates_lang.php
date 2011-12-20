4d17c595afbdd8c1d8a90e574b16d7cd645f37a2
cblair <cblair@vandals.uidaho.edu>
2011-12-19 21:26:49 -0800

    removed php

--- http/system/pyrocms/modules/templates/language/slovenian/templates_lang.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
-
-
-
-/* End of file templates_lang.php */
--- http/system/pyrocms/modules/themes/views/admin/partials/shortcuts.php
+++
@@ -1,8 +0,0 @@
-<nav id="shortcuts">
-	<h6><?php echo lang('cp_shortcuts_title'); ?></h6>
-	<ul>
-		<li><?php echo anchor('admin/themes/upload', lang('themes.upload_title'), 'class="add"'); ?></li>
-		<li><?php echo anchor('admin/themes', lang('themes.list_title')); ?></li>
-	</ul>
-	<br class="clear-both" />
-</nav>
