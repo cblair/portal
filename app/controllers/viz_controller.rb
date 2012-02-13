9c2a25e072c31147be196494f135d43850557242
Mike Solomon <solo4857@vandals.uidaho.edu>
2012-02-13 11:12:50 -0800

    Inserted highchart sample

--- app/controllers/viz_controller.rb
+++ app/controllers/viz_controller.rb
@@ -4,8 +4,8 @@ class VizController < ApplicationController
 
   def index
     #get the first Datum with the same param1 value
-    @test = Metadatum.find(params[:id])[:param1]
-    @data = Datum.where(:param1 => Metadatum.find(params[:id])[:param1])
+    #@test = Metadatum.find(params[:id])[:param1]
+    #@data = Datum.where(:param1 => Metadatum.find(params[:id])[:param1])
 =begin    
     @lc = GoogleChart::LineChart.new("400x200", "My Results", false)
     #@lc.data "Line green", [3,5,1,9,0,2], 'a9a9a9'
