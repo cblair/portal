4f4a187a724eb5a5da1bb863e78ec8d95a982085
Mike Solomon <solo4857@vandals.uidaho.edu>
2012-02-29 10:15:36 -0800

    Added chart example using lazy_high_charts

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,5 @@ require File.expand_path('../application', __FILE__)
 Portal::Application.initialize!
 
 ENV['RAILS_ENV'] ||= 'development'
+
+require 'lazy_high_charts'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Portal::Application.config.session_store :cookie_store, :key => '_portal_session'
+Portal::Application.config.session_store :cookie_store, key: '_portal_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
