921c6a25b95ff75b76ecbd43edd206daaf7c4f46
Mike Solomon <solo4857@vandals.uidaho.edu>
2012-03-29 20:42:43 -0700

    Added auto-login notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
 
     def autologin_if_dev
       if Rails.env.development? and not user_signed_in?
-          user = User.find_by_email('test@example.com')
-          sign_in user
+          development_user_email = 'test@example.com'
+          development_user = User.find_by_email(development_user_email)
+          sign_in development_user
+          flash[:notice] = "Automatically logged in as development-mode user #{development_user_email}"
       end
     end
 end
