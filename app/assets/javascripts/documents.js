14559a2a626e07fc69cee481b58596e4ad16a373
cblair <ecodata.cnr.uidaho@gmail.com>
2013-02-10 17:12:57 -0800

    disabling sort and search

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,9 +1,6 @@
 jQuery(function($) {
 
 	$.extend( $.fn.dataTableExt.oStdClasses, {
-		"sSortAsc": "header headerSortDown",
-		"sSortDesc": "header headerSortUp",
-		"sSortable": "header",
 		"sWrapper": "dataTables_wrapper form-inline"
 	});
 
@@ -13,7 +10,10 @@ jQuery(function($) {
 			"bJQueryUI"			: true,
 			"bProcessing"		: true,
 			"bServerSide"		: true,
-			"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+			"bSort"				: false,
+			//Disabling search
+			//"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+			"sDom": "<'row'<'span6'l><'span6'>r>t<'row'<'span6'i><'span6'p>>",
 			"sAjaxSource"		: $('#documents').data('source')
 		});
 	});
