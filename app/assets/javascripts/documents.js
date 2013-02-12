b34c4408501a97f8bc6fd22e33bc4d9251b959cf
cblair <ecodata.cnr.uidaho@gmail.com>
2013-02-11 17:14:19 -0800

    Going back to: Revert "disabling sort and search"
    This reverts commit 14559a2a626e07fc69cee481b58596e4ad16a373.

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,6 +1,9 @@
 jQuery(function($) {
 
 	$.extend( $.fn.dataTableExt.oStdClasses, {
+		"sSortAsc": "header headerSortDown",
+		"sSortDesc": "header headerSortUp",
+		"sSortable": "header",
 		"sWrapper": "dataTables_wrapper form-inline"
 	});
 
@@ -10,10 +13,7 @@ jQuery(function($) {
 			"bJQueryUI"			: true,
 			"bProcessing"		: true,
 			"bServerSide"		: true,
-			"bSort"				: false,
-			//Disabling search
-			//"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-			"sDom": "<'row'<'span6'l><'span6'>r>t<'row'<'span6'i><'span6'p>>",
+			"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 			"sAjaxSource"		: $('#documents').data('source')
 		});
 	});
