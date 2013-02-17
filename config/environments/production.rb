3bb6b84a0834fd367390c4dda3e38a1b0e0d4134
Colby Blair <cblair>
2013-02-17 15:55:21 -0800

    taking out default charset in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Portal::Application.configure do
   config.action_mailer.default_url_options = { :host => 'http://peaceful-lake-8763.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
+  #config.action_mailer.default_charset = "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
