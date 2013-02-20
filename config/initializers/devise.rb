abae1ce58dec0a4ccef60277b28fa5b1b9dbb76d
cblair <ecodata.cnr.uidaho@gmail.com>
2013-02-19 19:00:50 -0800

    Adding Hatch name to devise emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "ecodata.cnr.uidaho@gmail.com"
+  config.mailer_sender = "Hatch <ecodata.cnr.uidaho@gmail.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
