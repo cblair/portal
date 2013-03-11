5c849003ad0895b6e3f14c2a7fe9fbff89107078
shanesuofi <shanes@vandals.uidaho.edu>
2013-03-11 09:27:31 -0700

    Added basic unit tests for projects, started unit tests for projects helper.
    Signed-off-by: shanesuofi <shanes@vandals.uidaho.edu>

--- test/unit/helpers/projects_helper_test.rb
+++ test/unit/helpers/projects_helper_test.rb
@@ -1,4 +1,81 @@
 require 'test_helper'
+include CouchdbHelper
 
 class ProjectsHelperTest < ActionView::TestCase
+
+  include Devise::TestHelpers
+  
+	def setup
+		@request.env["devise.mapping"] = Devise.mappings[:admin]
+		@user = users(:user1)
+		sign_in @user
+	end
+
+	def teardown
+		sign_out @user
+
+		Collection.all.each do |c|
+			c.destroy
+		end
+
+		Document.all.each do |d|
+			d.destroy
+		end
+
+		Upload.all.each do |u|
+			u.destroy
+		end
+		
+		Project.all.each do |p|
+			p.destroy
+		end
+ 	end
+
+ 	test 'is_json?' do
+ 		assert is_json?('{ "test" : ["a", 1, "b"] }')
+ 		assert !is_json?('{ "test" : ["a", 1, "b"] : "c"}')
+ 		assert !is_json?(nil)
+ 	end
+
+
+	test "is_couchdb_running" do
+	    #Make sure CouchDB is running; even though it is not in projects_helper.rb
+	    result = is_couchdb_running?(
+	              host     = Portal::Application.config.couchdb['COUCHDB_HOST'], 
+	              port     = Portal::Application.config.couchdb['COUCHDB_PORT'],
+	              username = Portal::Application.config.couchdb['COUCHDB_USERNAME'],
+	              password = Portal::Application.config.couchdb['COUCHDB_PASSWORD'],
+	              https    = Portal::Application.config.couchdb['COUCHDB_HTTPS']
+	            )
+	    assert result
+ 	end
+
+  #test change owner
+  test "test_change_project_owner" do
+    proj = Project.new({:name => "Change Project Owner", :pdesc => "Change project owner test."})
+    proj.user = @user #set owner
+    #p("***", proj.user)
+    puts("------------------------------------------------------------")
+    
+    col = Collection(:one)
+    
+    Collection.each do |c|
+      puts("#{c.name}")
+    end
+=begin    
+    c=Collection.new(:name => "test_change_project_owner")
+	fname = 'ATM_small_test.zip'
+	upload = Upload.create(:name => fname, :upfile => File.open('test/unit/test_files/ATM_small_test.zip'))
+	
+	save_zip_to_documents(fname, upload, c, nil, @user)
+	
+	puts("------------------------------------------------------------")
+	puts("#{c.name}")
+	c.documents.each do |d|
+	  puts("************************************************************")
+	  p("#{c.name}")
+	end
+=end    
+  end
+ 	
 end
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,12 +1,75 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
-  def test_project_create
-    proj1 = Project.new({:name => "Test1", :pdesc => "This is test 1."})
+=begin
+   test "the truth" do
+     assert true
+   end
+=end
+=begin
+  include Devise::TestHelpers
+
+	def setup
+		@request.env["devise.mapping"] = Devise.mappings[:admin]
+		@user = users(:user1)
+		sign_in @user
+	end
+=end
+#=begin
+	def teardown
+		#sign_out @user
+
+		Collection.all.each do |c|
+			c.destroy
+		end
+
+		Document.all.each do |d|
+			d.destroy
+		end
+
+		Upload.all.each do |u|
+			u.destroy
+		end
+		
+		Project.all.each do |p|
+			p.destroy
+		end
+ 	end
+#=end
+  # test if couchDB is running?
+
+  # test create project
+  test "test_project_create" do
+    proj1 = Project.new({:name => "Create Project Test", :pdesc => "This is a test of create project."})
     assert proj1.valid?, "Needs a name and a description."
   end
+
+  #test project with bad name field
+  test "test_project_create_name_fail" do
+    proj = Project.new({:name => "Ab", :pdesc => "Is name valid test?"})
+    assert proj.invalid?, "Name field to short."
+  end
+  
+  #test project with bad description field
+  test "test_project_create_pdesc_fail" do
+    proj = Project.new({:name => "Description fail test.", :pdesc => ""})
+    assert proj.invalid?, "Description field to short."
+  end
+  
+  #test destroy project?
+
+  #test edit/update name of project
+  test "test_project_edit_name" do
+    proj = Project.new({:name => "Edit Project Name", :pdesc => "Edit project name test."})
+    proj.update_attributes(:name => "Edited Name")
+    assert proj.valid?, "Name may be to short."
+  end
+  
+  #test edit/update pdesc of project
+  test "test_project_edit_pdesc" do
+    proj = Project.new({:name => "Edit Project Pdesc", :pdesc => "Edit project pdesc."})
+    proj.update_attributes(:name => "Edited pdesc")
+    assert proj.valid?, "Description may be to short."
+  end
+
 end
